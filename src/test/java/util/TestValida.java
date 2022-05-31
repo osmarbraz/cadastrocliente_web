44c74889f59482bcdd92e198784b724edff608d0
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2022-05-31 11:17:32 -0300

    Aumentando a qualidade.

--- src/test/java/util/TestValida.java
+++ src/test/java/util/TestValida.java
@@ -10,39 +10,69 @@ public class TestValida {
 
     private Valida valida = null;
 
+    /**
+     * Instância uma classe de validação.
+     */
     @Before
     public void inicializa() {
         valida = new Valida();
     }
 
-    @After
-    public void finaliza() {
-        valida = null;
-    }
-
+    /**
+     * Testa CPF válido.
+     */
     @Test
     public void testValidaCPFValido1() {
         assertTrue(valida.validaCPF("11111111111"));
     }
 
+    /**
+     * Testa CPF válido.
+     */
     @Test
     public void testValidaCPFValido2() {
         assertTrue(valida.validaCPF("84807125206"));
     }
 
+    /**
+     * Testa CPF válido.
+     */
     @Test
     public void testValidaCPFValido3() {
         assertTrue(valida.validaCPF("63883136395"));
     }
 
+    /**
+     * Testa CPF válido.
+     */
     @Test
     public void testValidaCPFValido4() {
         assertTrue(valida.validaCPF("31626333033"));
     }
 
+    /**
+     * Testa CPF com problema na conversão.
+     */
     @Test
     public void testValidaCPFInvalido() {
         //CPF com problema na conversão
         assertFalse(valida.validaCPF("0065XAB22050"));
+    }
+    
+    /**
+     * Testa CPF com problema na quantidade de caracteres.
+     */
+    @Test
+    public void testValidaCPFInvalidoCurto() {
+        //CPF com problema na conversão
+        assertFalse(valida.validaCPF("111111"));
+    }
+
+    /**
+     * Finaliza a classe de validação.
+     */
+    @After
+    public void finaliza() {
+        valida = null;
     }
 }
